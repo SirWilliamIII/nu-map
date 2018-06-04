c169b81bb79d48a45381c0c070cefc298466289a
Will Carpenter <willcarpenteriii@gmail.com>
2018-06-03 23:18:53 -0500

    markers rendered to screen from json file

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Map } from 'google-maps-react'
+import { Map, Marker } from 'google-maps-react'
+import data from './data.json'
 
 class MapContainer extends Component {
 	state = {
@@ -7,6 +8,54 @@ class MapContainer extends Component {
 		activeMarker:      {},
 		selectedPlace:     []
 	}
+
+	createMarker = marker => {
+		return <Marker
+			key={ marker.id }
+			company={ marker.company }
+			street={ marker.street }
+			city={ marker.city }
+			state={ marker.state }
+			zipcode={ marker.zipcode }
+			lastInspection={ marker.lastInspection }
+			onClick={ this.onMarkerClick }
+			name={ marker.name }
+			position={ marker.position }/>
+	}
+
+	createAllMarkers(markers) {
+		return markers.map(marker => {
+			return this.createMarker(marker)
+		})
+	}
+	// filterByCompanyName(name) {
+	// 	this.state.displayedMarkers.push(this.createAllMarkers(data.vets).filter(vet => vet.props.company === name))
+	// }
+	//
+	// filterByLastInspectionDate(date) {
+	// 	this.state.displayedMarkers.push(this.createAllMarkers(data.vets).filter(vet => vet.props.lastInspection === date))
+	// }
+	//
+	// filterByCity(city) {
+	// 	this.state.displayedMarkers.push(this.createAllMarkers(data.vets).filter(vet => vet.props.city === city))
+	// }
+	//
+	// secondFilterByCompanyName(filter1, name) {
+	// 	this.state.displayedMarkers.push(filter1.filter(n => n.props.company === name))
+	// }
+	//
+	// secondFilterByLastInspectionDate(filter1, date) {
+	// 	this.state.displayedMarkers.push(filter1.filter(n => n.props.lastInspection === date))
+	// }
+	//
+	// secondFilterByCity(filter1, city) {
+	// 	this.state.displayedMarkers.push(filter1.filter(n => n.props.city === city))
+	// }
+	//
+	// filterByNameAndCity(name, city) {
+	// 	this.state.displayedMarkers.push(this.filterByCompanyName(name).filter(n => n.props.city === city))
+	// }
+
 	render() {
 		return (
 			<Map
@@ -18,6 +67,7 @@ class MapContainer extends Component {
 					lat: 31.16,
 					lng: -100.06
 				} }>
+			{ this.createAllMarkers(data.vets) }
 			</Map>
 		)
 	}
