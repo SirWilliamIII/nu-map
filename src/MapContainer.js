186afeec6766f2236b89ce9be3206292e5fd4602
Will Carpenter <willcarpenteriii@gmail.com>
2018-06-04 13:34:32 -0500

    added filters by name and city

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -6,7 +6,9 @@ class MapContainer extends Component {
 	state = {
 		showingInfoWindow: false,
 		activeMarker:      {},
-		selectedPlace:     []
+		selectedPlace:     [],
+		nameInput: '',
+		cityInput: ''
 	}
 
 	onMarkerClick = (props, marker, e) => {
@@ -19,7 +21,7 @@ class MapContainer extends Component {
 		}
 	}
 
-	onMapClick = props => {
+	onMapClick = (props) => {
 		if(this.state.showingInfoWindow) {
 			this.setState({
 				showingInfoWindow: false,
@@ -35,13 +37,26 @@ class MapContainer extends Component {
 		})
 	}
 
-	onWindowClose = () => {
+	onWindowClose = (props, marker, e) => {
 		this.setState({
 			showingInfoWindow: false
 		})
 	}
 
+	handleNameChange = e => {
+		this.setState({
+			nameInput: e.target.value
+		})
+	}
+
+	handleCityChange = e => {
+		this.setState({
+			cityInput: e.target.value
+		})
+	}
+
 	createMarker = marker => {
+		const pin_color = '7ec0ee'
 		return <Marker
 			key={ marker.id }
 			company={ marker.company }
@@ -52,7 +67,7 @@ class MapContainer extends Component {
 			lastInspection={ marker.lastInspection }
 			onClick={ this.onMarkerClick }
 			name={ marker.name }
-			icon={'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'}
+			icon={ `http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|${pin_color}` }
 			position={ marker.position }/>
 	}
 
@@ -62,8 +77,41 @@ class MapContainer extends Component {
 		})
 	}
 
+	filterByCompanyName(name) {
+		return this.createAllMarkers(data.vets).filter(vet => vet.props.company === name)
+	}
+
+	filterByCity(city) {
+		return this.createAllMarkers(data.vets).filter(vet => vet.props.city === city)
+	}
+
+	displayMarkers = () => {
+		let res
+		if(this.state.nameInput == '' && this.state.cityInput == '') {
+			res = this.createAllMarkers(data.vets)
+		} else if(this.state.nameInput){
+			res = this.filterByCompanyName(this.state.nameInput)
+		} else if(this.state.cityInput) {
+			res = this.filterByCity(this.state.cityInput)
+		}
+		return res
+	}
+
+
 	render() {
 		return (
+			<div>
+				<h3>Search By: </h3>
+					<div className="form-group">
+						<div className="row">
+							<div className="col-4">
+								<input className="form-control" type="text" placeholder="Company Name" value={ this.state.nameInput } onChange={ this.handleNameChange }/>
+							</div>
+							<div className="col-4">
+								<input className="form-control" type="text" placeholder="City" value={ this.state.cityInput } onChange={ this.handleCityChange }/>
+							</div>
+						</div>
+					</div>
 			<Map
 				google={ this.props.google }
 				style={ { width: '80%', height: '60%' } }
@@ -73,7 +121,8 @@ class MapContainer extends Component {
 					lat: 31.16,
 					lng: -100.06
 				} }>
-				{ this.createAllMarkers(data.vets) }
+
+				{ this.displayMarkers() }
 
 				<InfoWindow
 					marker={ this.state.activeMarker }
@@ -100,6 +149,7 @@ class MapContainer extends Component {
 					</div>
 				</InfoWindow>
 			</Map>
+		</div>
 		)
 	}
 }
