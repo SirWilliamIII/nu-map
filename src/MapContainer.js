5cb7fb7a4970106783f3449680da35fa62c53d87
Will Carpenter <willcarpenteriii@gmail.com>
2018-06-03 23:23:27 -0500

    added callbacks for click handlers

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Map, Marker } from 'google-maps-react'
+import { Map, Marker, InfoWindow } from 'google-maps-react'
 import data from './data.json'
 
 class MapContainer extends Component {
@@ -7,6 +7,38 @@ class MapContainer extends Component {
 		showingInfoWindow: false,
 		activeMarker:      {},
 		selectedPlace:     []
+	}
+
+	onMarkerClick = (props, marker, e) => {
+		if(this.state.selectedPlace) {
+			this.setState({
+				selectedPlace:     props,
+				activeMarker:      marker,
+				showingInfoWindow: true
+			})
+		}
+	}
+
+	onMapClick = props => {
+		if(this.state.showingInfoWindow) {
+			this.setState({
+				showingInfoWindow: false,
+				activeMarker:      null,
+				selectedPlace:     props
+			})
+		}
+	}
+
+	onWindowOpen = () => {
+		this.setState({
+			showingInfoWindow: true
+		})
+	}
+
+	onWindowClose = (props, marker, e) => {
+		this.setState({
+			showingInfoWindow: false
+		})
 	}
 
 	createMarker = marker => {
@@ -33,28 +65,14 @@ class MapContainer extends Component {
 	// }
 	//
 	// filterByLastInspectionDate(date) {
-	// 	this.state.displayedMarkers.push(this.createAllMarkers(data.vets).filter(vet => vet.props.lastInspection === date))
-	// }
-	//
-	// filterByCity(city) {
-	// 	this.state.displayedMarkers.push(this.createAllMarkers(data.vets).filter(vet => vet.props.city === city))
-	// }
-	//
-	// secondFilterByCompanyName(filter1, name) {
-	// 	this.state.displayedMarkers.push(filter1.filter(n => n.props.company === name))
-	// }
-	//
-	// secondFilterByLastInspectionDate(filter1, date) {
-	// 	this.state.displayedMarkers.push(filter1.filter(n => n.props.lastInspection === date))
-	// }
-	//
-	// secondFilterByCity(filter1, city) {
-	// 	this.state.displayedMarkers.push(filter1.filter(n => n.props.city === city))
-	// }
-	//
-	// filterByNameAndCity(name, city) {
-	// 	this.state.displayedMarkers.push(this.filterByCompanyName(name).filter(n => n.props.city === city))
-	// }
+	// 	this.state.displayedMarkers.push(this.createAllMarkers(data.vets).filter(vet => vet.props.lastInspection ===
+	// date)) }  filterByCity(city) { this.state.displayedMarkers.push(this.createAllMarkers(data.vets).filter(vet =>
+	// vet.props.city === city)) }  secondFilterByCompanyName(filter1, name) {
+	// this.state.displayedMarkers.push(filter1.filter(n => n.props.company === name)) }
+	// secondFilterByLastInspectionDate(filter1, date) { this.state.displayedMarkers.push(filter1.filter(n =>
+	// n.props.lastInspection === date)) }  secondFilterByCity(filter1, city) {
+	// this.state.displayedMarkers.push(filter1.filter(n => n.props.city === city)) }  filterByNameAndCity(name, city)
+	// { this.state.displayedMarkers.push(this.filterByCompanyName(name).filter(n => n.props.city === city)) }
 
 	render() {
 		return (
@@ -67,7 +85,32 @@ class MapContainer extends Component {
 					lat: 31.16,
 					lng: -100.06
 				} }>
-			{ this.createAllMarkers(data.vets) }
+				{ this.createAllMarkers(data.vets) }
+
+				<InfoWindow
+					marker={ this.state.activeMarker }
+					visible={ this.state.showingInfoWindow }
+					onOpen={ this.onWindowOpen }
+					onClose={ this.onWindowClose }
+				>
+					<div>
+						<a href="/"> <img alt={ '' }
+							src="https://www.poll-vaulter.com/wp-content/uploads/sites/5968/2017/12/whatever_Metro-News-UK.png"
+							height={ 32 } width={ 32 }/> </a>
+						<h3>Company: { this.state.selectedPlace.company }</h3>
+						<b>Address:</b>
+						<div>
+							{ this.state.selectedPlace.street } &nbsp;
+							{ this.state.selectedPlace.city }, &nbsp;
+							{ this.state.selectedPlace.state } &nbsp;
+							{ this.state.selectedPlace.zipcode }
+						</div>
+						<b>Last Inspection Date:</b>
+						<div>
+							{ this.state.selectedPlace.lastInspection }
+						</div>
+					</div>
+				</InfoWindow>
 			</Map>
 		)
 	}
